c17bee18f176cd89706718939564bc604c20b08f
PSPritish <pspkmahali@gmail.com>
2025-06-09 10:19:01 +0530

    jun 9 2025

---
+++ USACO/Problem_1_Shell_Game.cpp
@@ -0,0 +1,29 @@
+// Author: P S Pritish
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define ld long double
+#define endl "\n"
+constexpr int mod = 1e9 + 7;
+constexpr int inf = 1e18 + 1;
+
+void solve()
+{
+    
+}
+
+signed main()
+{
+    auto begin = std::chrono::high_resolution_clock::now();
+    cin.tie(0)->sync_with_stdio(0);
+    int t = 1;
+    // freopen("in",  "r", stdin);
+    // freopen("out", "w", stdout);
+    //  cin >> t;
+
+    while (t--)
+        solve();
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    // cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.<<endl;
+}
